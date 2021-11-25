19eee839eeba87e48942a5170e84d5daf6984ae0
ChechoCC <scanocolorado@outlook.com>
2021-11-24 21:02:54 -0500

    Update

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+const altura = document.body.scrollHeight = window.innerHeight; 
+const fondo = document.getElementById('fondo');
+
+window.onscroll = () => {
+    const anchoFondo = (window.pageYOffset / altura) * 700;
+    fondo.style.width = anchoFondo + '%';  
+}
